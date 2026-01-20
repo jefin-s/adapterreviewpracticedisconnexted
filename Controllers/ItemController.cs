4f7ebb8b5eca5691f27c5fdf5a2b7657fb797fec
jefin-s <jefin.s786@gmail.com>
2026-01-20 14:20:54 +0530

    sp with out parameter

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using adapterreviewpractice.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 using System.Data;
 
 namespace adapterreviewpractice.Controllers
@@ -30,8 +31,8 @@ namespace adapterreviewpractice.Controllers
                 adapter.Fill(ds, "Products");
                 DataTable dt = ds.Tables["Products"];
                 DataView dv = new DataView(dt);
-                dv.RowFilter = "description='string'";
-                dv.Sort = "price desc";
+                //dv.RowFilter = "description='string'";
+                //dv.Sort = "price desc";
                 //foreach (DataRowView row in dv)
                     foreach (DataRow row in dt.Rows)
                 {
@@ -136,7 +137,90 @@ namespace adapterreviewpractice.Controllers
 
             return Ok("Item deleted successfully!");
         }
+        [HttpGet("storedprocedure")]
+        public IActionResult GetItemsUsingStoredProcedure()
+        {
+            List<Items> items = new List<Items>();
+            string connectionString = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("showprodcct", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                   SqlDataAdapter da= new SqlDataAdapter(cmd);
 
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "Products");
+                    DataTable dt = ds.Tables["Products"];
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        items.Add(new Items
+                        {
+                            Id = (int)row["id"],
+                            name = row["name"].ToString(),
+                            description = row["description"].ToString()
+                        });
+                    }
+                }
+            }
+            return Ok(items);
+        }
+        [HttpPost("sp add")]
+        public IActionResult addsp(Items newitem)
+        {
+            string connection = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con=new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand( "addprodust",con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@name", newitem.name);
+                cmd.Parameters.AddWithValue("@price", newitem.price);
+                cmd.Parameters.AddWithValue("@description", newitem.description);
+                cmd.Parameters.AddWithValue("@products_makkers", newitem.product_makers);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return Ok("added successfully");
+        }
+        [HttpPut("id")]
+        public IActionResult updaet(int id,Items updateitem)
+        {
+                string connection = _config.GetConnectionString("DefaultConnection");
+                using (SqlConnection con = new SqlConnection(connection))
+                {
+                    SqlCommand cmd = new SqlCommand("up", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@name", updateitem.name);
+                
+                con.Open();
+                int row=cmd.ExecuteNonQuery();
+            }
+            return Ok("added successfully");
+        }
+        [HttpGet("TotalProducts")]
+        public IActionResult TotalaProducts()
+        {
+            int totalcount = 0;
+            string cin = _config.GetConnectionString("Defaultconnection");
+            using (SqlConnection con = new SqlConnection(cin))
+            {
+             
+                SqlCommand cmd = new SqlCommand("tot", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter outputparams = new SqlParameter("@total", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(outputparams);
+                con.Open();
+                cmd.ExecuteNonQuery();
+              totalcount = (int)cmd.Parameters["@total"].Value;
+                return Ok(totalcount);
+            }
+        }
 
     }
 }
